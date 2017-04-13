b6a541dd91b21e938520777462892aeed047a903
andyreidz <andrew.reid@hpe.com>
2017-04-13 21:08:28 +0100

    Update sections.js

--- assets/sections.js
+++ assets/sections.js
@@ -93,14 +93,14 @@ angular.module('cafapi.templates', []).run(['$templateCache', function($template
   );
 
 
-  $templateCache.put('showcase/en-us/services/elements.md',
-    "Elements is the only open source user interface framework for building modern, responsive, mobile big data applications on the web. \r" +
+  $templateCache.put('showcase/en-us/services/aspects.md',
+    "Aspects is the only open source user interface framework for building modern, responsive, mobile big data applications on the web. \r" +
     "\n" +
     "\r" +
     "\n" +
-    "Elements provides all of the essentials required for your big data project, allowing you to swiftly create dashboards and reports that provide users with the\r" +
+    "Aspects provides all of the essentials required for your big data project, allowing you to swiftly create dashboards and reports that provide users with the\r" +
     "\n" +
-    "insights they crave from their big data applications. Elements is based on best-of-breed, open source technologies, including AngularJS,\r" +
+    "insights they crave from their big data applications. Aspects is based on best-of-breed, open source technologies, including Angular 4,\r" +
     "\n" +
     "Bootstrap and D3. It is easy to include in your web applications with the Bower web package manager.\r" +
     "\n"
